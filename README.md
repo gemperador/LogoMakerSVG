7aba7866dd684703e866482e59b67f22bf9083cd
gemperador <gabriemperador@gmail.com>
2024-06-10 19:00:38 -0700

    it works! added readme

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # LogoMakerSVG
+
+## Description
+
+The Logo Maker is a CLI application that generates SVG logos based on user input. Users can customize the logo's text, text color, shape, and shape color. The generated logo is saved as an logo.svg file.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Links](#links)
+- [Questions](#questions)
+
+## Installation
+
+To install, clone repo and install necessary dependencies.
+
+## Usage
+
+To use the Logo Maker, run the following command in your terminal: node index.js
+
+## License
+
+This project is licensed under the MIT license.
+
+## Contributing
+
+Gabby
+
+## Screenshots
+
+## Links
+
+Link to deployed app:: (https://gemperador.github.io/LogoMakerSVG/) 
+
+ Link to repo: (https://github.com/gemperador/LogoMakerSVG) 
+
+## Questions
+
+For any questions, you can contact me via:
+
+- GitHub: [gemperador](https://github.com/gemperador)
+
+- Email: [email](gabriemperador@gmail.com)
--- index.js
+++ index.js
@@ -1,59 +1,68 @@
 // index.js
-const fs = require('fs');
-const inquirer = require('inquirer');
-const { Circle, Triangle, Square } = require('./lib/shapes');
+async function run() {
+  const fsModule = await import('fs');
+  const inquirerModule = await import('inquirer');
+  const shapesModule = await import('./lib/shapes.cjs');
 
-const questions = [
-  {
-    type: 'input',
-    name: 'text',
-    message: 'Enter up to three characters for the logo:',
-    validate: input => input.length <= 3 || 'Please enter up to three characters only.',
-  },
-  {
-    type: 'input',
-    name: 'textColor',
-    message: 'Enter the text color (keyword or hexadecimal):',
-  },
-  {
-    type: 'list',
-    name: 'shape',
-    message: 'Choose a shape:',
-    choices: ['Circle', 'Triangle', 'Square'],
-  },
-  {
-    type: 'input',
-    name: 'shapeColor',
-    message: 'Enter the shape color (keyword or hexadecimal):',
-  },
-];
+  const fs = fsModule.promises; // Use fs promises for async file operations
+  const inquirer = inquirerModule.default;
+  const { Circle, Triangle, Square } = shapesModule;
 
-function generateSVG({ text, textColor, shape, shapeColor }) {
-  let shapeElement;
+  const questions = [
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters for the logo:',
+      validate: input => input.length <= 3 || 'Please enter up to three characters only.',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter the text color (keyword or hexadecimal):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['Circle', 'Triangle', 'Square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter the shape color (keyword or hexadecimal):',
+    },
+  ];
 
-  switch (shape) {
-    case 'Circle':
-      shapeElement = new Circle(shapeColor);
-      break;
-    case 'Triangle':
-      shapeElement = new Triangle(shapeColor);
-      break;
-    case 'Square':
-      shapeElement = new Square(shapeColor);
-      break;
+  function generateSVG({ text, textColor, shape, shapeColor }) {
+    let shapeElement;
+
+    switch (shape) {
+      case 'Circle':
+        shapeElement = new Circle(shapeColor);
+        break;
+      case 'Triangle':
+        shapeElement = new Triangle(shapeColor);
+        break;
+      case 'Square':
+        shapeElement = new Square(shapeColor);
+        break;
+    }
+
+    const svgContent = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shapeElement.render()}
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+      </svg>
+    `;
+
+    fs.writeFile('logo.svg', svgContent.trim())
+      .then(() => console.log('Generated logo.svg'))
+      .catch(err => console.error('Error writing file:', err));
   }
 
-  const svgContent = `
-    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      ${shapeElement.render()}
-      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-    </svg>
-  `;
-
-  fs.writeFileSync('logo.svg', svgContent.trim());
-  console.log('Generated logo.svg');
+  inquirer.prompt(questions).then(answers => {
+    generateSVG(answers);
+  });
 }
 
-inquirer.prompt(questions).then(answers => {
-  generateSVG(answers);
-});
+run();
